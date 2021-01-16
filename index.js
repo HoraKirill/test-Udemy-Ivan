bbc088a37752380523d2a2c1957487c500b64efa
Kirill <telo2005@mail.ru>
2021-01-16 13:32:17 +0300

    Второй коммит

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const  pi = 3.14;
-console.log (pi);
-
-let persone = {
-    name: "John",
-    age: 25,
-    ismarried: false
+let appData = {
+    allMoney: 0,
+    timeData: "",
+    expenses: {
+        v1: '',
+        v2: ''
+    },
+    optionalExpenses: 0,
+    income: [],
+    savings: false
 };
-console.log (persone.name);
+var money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+appData.allMoney = money;
+appData.timeData = time;
+appData.expenses.v1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+appData.expenses.v2 = prompt("Во сколько обойдется?", "");
+alert('бюджет на 1 день: ' + appData.allMoney / 30);
+console.log(appData);
