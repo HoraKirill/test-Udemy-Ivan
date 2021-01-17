8102c2ae85a0f5124e25452f4396372af585a4f1
Kirill <telo2005@mail.ru>
2021-01-17 14:46:07 +0300

    17.01.2020

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+let money = +prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 let appData = {
-    allMoney: 0,
-    timeData: "",
-    expenses: {
-        v1: '',
-        v2: ''
-    },
-    optionalExpenses: 0,
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
     income: [],
     savings: false
 };
-var money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-appData.allMoney = money;
-appData.timeData = time;
-appData.expenses.v1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-appData.expenses.v2 = prompt("Во сколько обойдется?", "");
-alert('бюджет на 1 день: ' + appData.allMoney / 30);
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) !=null
+    && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i=0;
+    }
+};
+appData.moneyPerDay = appData.budget / 30;
+alert('бюджет на 1 день: ' + appData.moneyPerDay);
+
 console.log(appData);
+
+var funct = (n) => {
+    // Program Me
+    return n.toString(2).replace(/0/g,'').length;
+  };
+  funct (4);
+
+ var countBits = n => n.toString(2).split('0').join('').length;
+  countBits(4);
